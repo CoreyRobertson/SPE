5f48021f22674af4230e8315d1a18e3bf8000bc6
coreyr <coreystephenr@gmail.com>
2024-05-20 13:19:19 +1000

    next commit

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -9,7 +9,7 @@ const Reviews = () => {
     return (
         <section className='reviews_section'>
             <div className='intro_background-container'>
-                <Image src='/images/Hero2.svg' alt='Hero Background' layout='fill' className='intro_background' />
+                <Image src='/images/Hero.jpg' alt='Hero Background' layout='fill' className='intro_background' quality={90} />
             </div>
             <div className='reviews_text-section'>
                 <h4 className='main_heading'>What do our clients have to say <span> about us and our services</span></h4>
